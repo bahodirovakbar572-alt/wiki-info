999d3e57473161fdbd9fa5b2897907300655416d
Akbar Bahodirov <bahodirovakbar572@gmail.com>
2026-06-07 08:48:42 +0500

    first commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 let currentSearchResults = [];
-const API_BASE_URL = 'https://wiki-info.onrender.com';
+const API_BASE_URL = 'https://wiki-uz.onrender.com';
 
 function apiUrl(path) {
     return `${API_BASE_URL.replace(/\/+$/, '')}/${path.replace(/^\/+/, '')}`;
